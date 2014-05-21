1e4e8b50aeda89bfa3712b4dd06d9430305fae98
Daniel Groves <hello@danielgroves.net>
2014-05-21 12:03:22 +0100

    Add configuration and ansible DB migrations

--- app/vagranttest/vagranttest/settings.py
+++ app/vagranttest/vagranttest/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'South',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -57,8 +58,11 @@ WSGI_APPLICATION = 'vagranttest.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'django_app',
+        'USER': 'django',
+        'PASSWORD': 'sdjgh34iutwefhfgbqkj3',
+        'HOST': 'localhost',
     }
 }
 
